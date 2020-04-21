3c38b437bbdf1c07cc93fc60f962951d7c555f39
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-21 05:37:59 +0530

    export data from csv from confirmed table

--- src/data/processing/confirmed.js
+++ src/data/processing/confirmed.js
@@ -1,6 +1,8 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/confirm.js");
+const map = require("/Users/bhagyalakshmi/Documents/COVID_19/src/data/processing/mapping.js");
+const mapp = map.Data;
 const Confirm = confirm.Confirm;
 fs.createReadStream(
   "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmed_cases_in_global.csv"
@@ -8,6 +10,7 @@ fs.createReadStream(
   .pipe(csv())
   .on("data", row => {
     country = row.country;
+    result = mapp.country;
     total_cases = row.total_cases;
     Confirm.sync()
       .then(() => {
