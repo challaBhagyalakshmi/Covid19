7579948e89d63074f0a74222b35562152af55671
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-23 01:05:58 +0530

    top 10 countries who is having highest death rates

--- src/api/routes/deaths_route.js
+++ src/api/routes/deaths_route.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const sequelize = require("sequelize");
+const Sequelize = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
 const router = express.Router();
-const deaths = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/deaths.js");
+const sequelize = Sequelize.sequelize;
 
 router.get("/top10", (req, res) => {
   sequelize
-    .query("select * from recovered_cases order by no_of_cases asc limit 10")
+    .query(
+      "select country_name,no_of_cases from countries c,deaths d where c.country_code=d.country_code order by no_of_cases asc limit 10"
+    )
     .then(data => {
       res.send(JSON.stringify(data));
       res.status(200);
