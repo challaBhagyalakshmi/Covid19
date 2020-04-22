38b9432ef244e7b4c6a7c7c081540ea37ea066dd
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-23 01:04:00 +0530

    top 10 no of confirmed cases in world wide

--- src/api/routes/confirm_router.js
+++ src/api/routes/confirm_router.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const sequelize = require("sequelize");
+const Sequelize = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
 const router = express.Router();
-const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/confirm.js");
+const sequelize = Sequelize.sequelize;
 
 router.get("/top10", (req, res) => {
   sequelize
-    .query("select * from confirm_cases order by no_of_cases asc limit 10")
+    .query(
+      "select country_name,no_of_cases from countries c,confirm_cases d where c.country_code=d.country_code order by no_of_cases asc limit 10"
+    )
     .then(data => {
       res.send(JSON.stringify(data));
       res.status(200);
