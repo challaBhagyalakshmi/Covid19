3957bd9017645f38667fd05df2ebfdd42edffe93
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-23 01:20:08 +0530

    middlewares

---
+++ src/api/Middlewares/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/confirm_router.js");
+const recover = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/recover_route.js");
+const deaths = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/deaths_route.js");
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/confirmed", confirm);
+app.use("/recovered", recover);
+app.use("/deaths", deaths);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,DELETE");
+  next();
+});
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  res.status = 404;
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: error.message
+  });
+});
+
+module.exports = app;
