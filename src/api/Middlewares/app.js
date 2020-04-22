abedd709e758a5b2e9c189d8aca0a01a74895401
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-23 01:49:57 +0530

    middlewares

--- src/api/Middlewares/app.js
+++ src/api/Middlewares/app.js
@@ -6,13 +6,16 @@ const bodyParser = require("body-parser");
 const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/confirm_router.js");
 const recover = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/recover_route.js");
 const deaths = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/deaths_route.js");
-
+const login = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/login.js");
+const signup = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/signup.js");
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/confirmed", confirm);
 app.use("/recovered", recover);
 app.use("/deaths", deaths);
+app.use("/signup", signup);
+app.use("/login", login);
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
