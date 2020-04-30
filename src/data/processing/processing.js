0d6f53b1eff7995f0f11b81026fd1e86aa4bdb9e
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-30 06:40:05 +0530

    data export from countries table

--- src/data/processing/processing.js
+++ src/data/processing/processing.js
@@ -1,24 +1,23 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-//const sequelize = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
 const countries = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/countries.js");
 const Country = countries.country;
 fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/countries.csv"
+  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/countries.csv"
 )
   .pipe(csv())
   .on("data", row => {
-    data = Object.values(row);
+    const data = Object.values(row);
+    const dat = data[0];
     Country.sync()
       .then(function() {
         return Country.create({
-          country_name: data[0]
+          country_name: dat
         });
       })
       .then(data => {
         console.log(data);
       });
-    //console.log(data[0]);
   })
   .on("end", () => {
     console.log("csv file successfully processed");
