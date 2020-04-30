a53099afc129996ea83bcb8e51a052ef99f0cb25
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-30 06:56:39 +0530

    create new user

--- src/api/routes/signup.js
+++ src/api/routes/signup.js
@@ -3,26 +3,25 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
 const User = user.User;
-router.post("/signup", (req, res) => {
-  if (!req.body.username || !req.body.password || !req.body.email) {
-    res.status(400).send({
-      message: "please enter username,password and email"
-    });
-  } else {
-    User.create({
-      name: req.body.username,
-      pass: req.body.password,
-      email: req.body.email
-    })
-      .then(user => {
-        res.status(200).send(user);
-      })
-      .catch(error => {
-        res.status(400).send({
-          error: error.message
+
+router.post("/signup", async (req, res) => {
+  const pwd = req.body.pass;
+  const hashed = await bcrypt.hash(pwd, 8);
+  try {
+    User.sync()
+      .then(function() {
+        return User.create({
+          name: req.body.username,
+          pass: hashed,
+          email: req.body.email
         });
+      })
+      .then(data => {
+        res.status(200);
+        res.send(JSON.stringify(data));
       });
+  } catch (error) {
+    console.log(error);
   }
 });
-
 module.exports = router;
