05f00438926d2ccc01a46f99263563f80527beb3
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-04-30 06:41:23 +0530

    data export from csv to recovered table

--- src/data/processing/recovered_export.js
+++ src/data/processing/recovered_export.js
@@ -1,26 +1,22 @@
 const csv = require("csv-parser");
 const fs = require("fs");
 const recovered = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/recovered.js");
-const map = require("/Users/bhagyalakshmi/Documents/COVID_19/src/data/processing/mapping.js");
-const mapp = map.Data;
 const Recover = recovered.Recover;
 fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/recovered_global.csv"
+  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/recovered.csv"
 )
   .pipe(csv())
   .on("data", row => {
-    country = row.country;
-    result = mapp.country;
-    total_cases = row.total_cases;
+    const data = row["4/28/20"];
     Recover.sync()
-      .then(() => {
+      .then(function() {
         return Recover.create({
-          country_code: country,
-          No_of_cases: total_case
+          "4/28/20": data
         });
       })
-      .then(data => {
-        console.log(data);
+      .then(dat => {
+        console.log(JSON.stringify(dat));
       });
   })
   .on("end", () => console.log("csv file successfully processed"));
+///Users/bhagyalakshmi/Documents/COVID_19/src/data/processing/recovered_export.js
