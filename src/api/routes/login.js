fae836b88b3e98823cf49b210f8082d8c8d22ae8
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-02 06:44:17 +0530

    logging the user

--- src/api/routes/login.js
+++ src/api/routes/login.js
@@ -3,36 +3,39 @@ const bcrypt = require("bcrypt");
 const router = express.Router();
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
 const User = user.User;
-router.post("/login", (req, res) => {
-  User.find({
-    where: {
-      username: req.body.username
-    }
-  })
-    .then(user => {
-      if (!user) {
-        return res.status(401).send({
-          message: "User not found."
-        });
-      }
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if (isMatch && !err) {
-          var token = jwt.sign(JSON.parse(JSON.stringify(user)), "secret", {
-            expiresIn: 10000
-          });
-          jwt.verify(token, "coivid", (err, data) => {
-            console.log(err, data);
-          });
-          res.json({ success: true, token: "JWT " + token });
-        } else {
-          res.status(401).send({
-            success: false,
-            msg: "Authentication failed."
-          });
-        }
-      });
-    })
-    .catch(error => res.status(400).send(error));
+router.post("/login", auth,(req, res) => {
+  try {
+    const user = credential(req.body.email, req.body.pass);
+    const token = await generatetoken();
+    res.send({ user, token });
+    res.send(user);
+    res.status(200);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
+
+async function credential(email, pass) {
+  const user = sequelize.sync().then(function() {
+    return User.findAll({
+      where: {
+        email: email
+      }
+    });
+  });
+  if (!user) {
+    throw new Error("login failed");
+  }
+  const pwd = await bcrypt.hash(pass, 8);
+  const isMatch = await bcrypt.compare(pwd, user.pass);
+  if (!isMatch) {
+    throw new Error("login failed");
+  }
+  return user;
+}
+async function generatetoken(user) {
+  const token = jwt.sign({ id: user.id }, "covid");
+  return token;
+}
 
 module.exports = { router };
