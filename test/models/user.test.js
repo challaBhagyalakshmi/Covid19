7d3cf2b66c5909101d10c976542eb823121b6010
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-02 07:09:30 +0530

    testcases for user model

--- test/models/user.test.js
+++ test/models/user.test.js
@@ -1,5 +1,6 @@
 const connection = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
+const bcrypt = require("bcrypt");
 const User = user.User;
 const sequelize = connection.sequelize;
 
@@ -8,17 +9,19 @@ describe("User model ", () => {
     sequelize.drop({ force: true });
   });
   it("inserting a new record ", () => {
+    const pwd = "pass456";
+    const hashed = bcrypt.hash(pwd, 8);
     User.sync()
       .then(function() {
         return User.create({
           name: "user2",
-          pass: "pass456",
+          pass: hashed,
           email: "user2@gmail.com"
         });
       })
       .then(data => {
         expect(data.name).toBe("user2");
-        expect(data.pass).toBe("pass456");
+        expect(data.pass).toBe(hashed);
         expect(data.pass).toBe("user2@gmail.com");
         expect(data.admin).toBe(false);
       });
