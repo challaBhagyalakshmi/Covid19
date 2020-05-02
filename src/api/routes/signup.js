55b8574dfb12f670da92e3e5afed0a6b1e419b58
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-02 06:45:41 +0530

    create new user

--- src/api/routes/signup.js
+++ src/api/routes/signup.js
@@ -1,27 +1,37 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+const connection = require("");
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
 const User = user.User;
 
 router.post("/signup", async (req, res) => {
   const pwd = req.body.pass;
   const hashed = await bcrypt.hash(pwd, 8);
-  try {
-    User.sync()
-      .then(function() {
-        return User.create({
-          name: req.body.username,
-          pass: hashed,
-          email: req.body.email
+  const email = req.body.email;
+  const user = sequelize.sync().then(function() {
+    return User.findOne(email);
+  });
+  if (!user) {
+    res.send("user is already registered");
+  } else {
+    try {
+      User.sync()
+        .then(function() {
+          return User.create({
+            name: req.body.username,
+            pass: hashed,
+            email: req.body.email
+          });
+        })
+        .then(data => {
+          res.status(200);
+          res.send(JSON.stringify(data));
         });
-      })
-      .then(data => {
-        res.status(200);
-        res.send(JSON.stringify(data));
-      });
-  } catch (error) {
-    console.log(error);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
 module.exports = router;
