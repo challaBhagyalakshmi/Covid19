ce27e91bff029ed1d6be67628dfb17d8ebd6ffef
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-08 18:22:35 +0530

    top10 countries who is having highest number of covid deaths in world wide

--- src/api/routes/deaths_route.js
+++ src/api/routes/deaths_route.js
@@ -6,15 +6,14 @@ const sequelize = Sequelize.sequelize;
 router.get("/top10", (req, res) => {
   try {
     sequelize
-    .query(
-      "select country_name,no_of_cases from countries c,deaths d where c.country_code=d.country_code order by "4/28/20" desc limit 10"
-    )
-    .then(data => {
-      res.send(JSON.stringify(data));
-      res.status(200);
-    });
-  }
-  catch (error) {
+      .query(
+        "select country_name,no_of_cases from countries c,deaths d where c.id=d.country_code order by 4/28/20 desc limit 10"
+      )
+      .then((data) => {
+        res.send(JSON.stringify(data));
+        res.status(200);
+      });
+  } catch (error) {
     console.log(error);
   }
 });
