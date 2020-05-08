49fa15992b7be289bac96982d6112062a88dba62
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-08 18:32:10 +0530

    data import to foreignkeys attribute to deaths table

--- src/db/Models/Foreignkeys/deaths.js
+++ src/db/Models/Foreignkeys/deaths.js
@@ -1,40 +1,45 @@
 const csv = require("csv-parser");
 const Sequelize = require("sequelize");
+const fs = require("fs");
 const deaths = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/deaths.js");
 const country = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/countries.js");
 const connection = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
 const Country = country.country;
 const Deaths = deaths.Death;
 const sequelize = connection.sequelize;
+var count = 0;
 fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmed.csv"
+  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmes.csv"
 )
   .pipe(csv())
-  .on("data", (row) => {
-    const data = row.Country;
-    sequelize.sync().then(function () {
-      return Country.findAll({
-        where: {
-          country_name: data,
-        },
-        attributes: ["id"],
-      }).then((id) => {
-        const id_val = id.id;
-        sequelize.sync().then(function () {
-          Deaths.update(
+  .on("data", async (row) => {
+    const country = await row.Country;
+    sequelize
+      .sync()
+      .then(function () {
+        return Country.findAll({
+          where: {
+            country_name: country,
+          },
+        });
+      })
+      .then(async (data) => {
+        const val = await data[0].id;
+        sequelize.sync().then(async function () {
+          await Deaths.update(
             {
-              country_code: id_val,
+              country_code: val,
             },
             {
               where: {
-                country_name: data,
+                country_name: country,
               },
             }
           );
         });
       });
-    });
   })
   .on("end", () => {
     console.log("successfully csv is processed");
+    console.log(count);
   });
