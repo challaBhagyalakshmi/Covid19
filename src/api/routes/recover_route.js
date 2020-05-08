14c7d2f966c13736b6712b7a04b338ac38478eb8
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-08 18:24:08 +0530

    top10 countries who is having highest number covid recover cases

--- src/api/routes/recover_route.js
+++ src/api/routes/recover_route.js
@@ -6,15 +6,14 @@ const sequelize = Sequelize.sequelize;
 router.get("/top10", (req, res) => {
   try {
     sequelize
-    .query(
-      "select country_name,no_of_cases from recovered_cases r,countries c where c.country_code=r.country_code order by "4/28/20" desc limit 10"
-    )
-    .then(data => {
-      res.send(JSON.stringify(data));
-      res.status(200);
-    });
-  }
-  catch (error) {
+      .query(
+        "select country_name,no_of_cases from recovered_cases r,countries c where c.id=r.country_code order by 4/28/20 desc limit 10"
+      )
+      .then((data) => {
+        res.send(JSON.stringify(data));
+        res.status(200);
+      });
+  } catch (error) {
     console.log(error);
   }
 });
