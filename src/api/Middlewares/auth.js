cf717d8439019b7ca3bd99cc0908048c90636b3a
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-11 17:18:54 +0530

    authorization

--- src/api/Middlewares/auth.js
+++ src/api/Middlewares/auth.js
@@ -1,16 +1,18 @@
 const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
 const User = user.User;
 const auth = async (req, res, next) => {
   try {
-    const token = req.header("Authprization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "covid19");
-    const user = await User.findOne({ id: decoded.id });
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.secret_key);
+    const user = await User.findAll({ id: decoded.id });
 
     if (!user) {
       throw new Error();
     }
-    req.user = user;
+    //req.user = user;
     next();
   } catch (error) {
     res.staus(401).send({ error: "please authenticate" });
