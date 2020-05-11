a7c6422dd1703b740f6c3f3226d7072ac08c14f0
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-11 17:23:24 +0530

    export data from csv to confirm_table

--- src/data/processing/confirmed.js
+++ src/data/processing/confirmed.js
@@ -1,23 +1,34 @@
 const csv = require("csv-parser");
 const fs = require("fs");
+const foreign_key = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/Foreignkeys/confirm.js");
 const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/confirm.js");
 const Confirm = confirm.Confirm;
-fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmed.csv"
-)
-  .pipe(csv())
-  .on("data", row => {
-    const data = row["4/28/20"];
-    Confirm.sync()
-      .then(function() {
-        return Confirm.create({
-          "4/28/20": data
+
+Confirm.destroy({});
+async function confirm_data_import_db() {
+  fs.createReadStream(
+    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmes.csv"
+  )
+    .pipe(csv())
+    .on("data", (row) => {
+      const data = row["4/28/20"];
+      const coun_name = row.Country;
+      Confirm.sync()
+        .then(function () {
+          return Confirm.create({
+            "4/28/20": data,
+            country_name: coun_name,
+          });
+        })
+        .then((dat) => {
+          console.log(JSON.stringify(dat));
         });
-      })
-      .then(dat => {
-        console.log(JSON.stringify(dat));
-      });
-  })
-  .on("end", () => {
-    console.log("csv file successfully processed");
-  });
+      console.log(row);
+      foreign_key.foreign_confirm();
+    })
+    .on("end", () => {
+      console.log("csv file successfully processed");
+    });
+}
+
+module.exports = { confirm_data_import_db };
