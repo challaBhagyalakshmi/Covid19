129e6d02704ccf7bb0cdd30b05c20420f86c1fef
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-11 17:21:20 +0530

    Authenticating the user

--- src/api/routes/login.js
+++ src/api/routes/login.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
 const router = express.Router();
 const user = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/user.js");
 const auth = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/Middlewares/auth.js");
@@ -8,7 +10,7 @@ const User = user.User;
 router.post("/login", Auth, async (req, res) => {
   try {
     const user = credential(req.body.email, req.body.pass);
-    const token = await generatetoken();
+    const token = await generatetoken(user);
     res.send({ user, token });
     res.send(user);
     res.status(200);
@@ -36,7 +38,7 @@ async function credential(email, pass) {
   return user;
 }
 async function generatetoken(user) {
-  const token = jwt.sign({ id: user.id }, "covid");
+  const token = jwt.sign({ id: user.id }, process.env.secret_key);
   return token;
 }
 
