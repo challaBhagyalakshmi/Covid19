eacb745d5feb1f9caeb6fb840962e733c1d0841d
Bhagyalakshmi <bhagyalakshmi@Administrators-MacBook-Pro.local>
2020-05-11 17:45:52 +0530

    foreign key attribute in confirm_cases

--- src/db/Models/Foreignkeys/confirm.js
+++ src/db/Models/Foreignkeys/confirm.js
@@ -7,39 +7,42 @@ const connection = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/confi
 const Country = country.country;
 const Confirm = confirm.Confirm;
 const sequelize = connection.sequelize;
-var count = 0;
-fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmes.csv"
-)
-  .pipe(csv())
-  .on("data", async (row) => {
-    const country = await row.Country;
-    sequelize
-      .sync()
-      .then(function () {
-        return Country.findAll({
-          where: {
-            country_name: country,
-          },
-        });
-      })
-      .then(async (data) => {
-        const val = await data[0].id;
-        sequelize.sync().then(async function () {
-          await Confirm.update(
-            {
-              country_code: val,
+async function foreign_confirm() {
+  fs.createReadStream(
+    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmes.csv"
+  )
+    .pipe(csv())
+    .on("data", async (row) => {
+      const country = await row.Country;
+      sequelize
+        .sync()
+        .then(function () {
+          return Country.findAll({
+            where: {
+              country_name: country,
             },
-            {
-              where: {
-                country_name: country,
+          });
+        })
+        .then(async (data) => {
+          const val = await data[0].id;
+          sequelize.sync().then(async function () {
+            await Confirm.update(
+              {
+                country_code: val,
               },
-            }
-          );
+              {
+                where: {
+                  country_name: country,
+                },
+              }
+            );
+          });
         });
-      });
-  })
-  .on("end", () => {
-    console.log("successfully csv is processed");
-    console.log(count);
-  });
+    })
+    .on("end", () => {
+      console.log("successfully csv is processed");
+      console.log(count);
+    });
+}
+
+module.exports = { foreign_confirm };
