819f9f6e5617514cbd93eac7fadcee2c081ad5f3
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:28:53 +0530

    data updation of foreignkey attribute in recovered table

--- src/db/Models/Foreignkeys/recover.js
+++ src/db/Models/Foreignkeys/recover.js
@@ -1,17 +1,16 @@
 const csv = require("csv-parser");
 const Sequelize = require("sequelize");
 const fs = require("fs");
-const recover = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/recovered.js");
-const country = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/countries.js");
-const connection = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/config/connection.js");
+const recover = require("../recovered.js");
+const country = require("../countries.js");
+const connection = require("../../config/connection.js");
+
 const Country = country.country;
 const Recover = recover.Recover;
 const sequelize = connection.sequelize;
-var count = 0;
+
 async function foreign_key_recover() {
-  fs.createReadStream(
-    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/recovered.csv"
-  )
+  fs.createReadStream("../../../data/csv_files/recovered.csv")
     .pipe(csv())
     .on("data", async (row) => {
       const country = await row.Country;
@@ -42,7 +41,6 @@ async function foreign_key_recover() {
     })
     .on("end", () => {
       console.log("successfully csv is processed");
-      console.log(count);
     });
 }
 
