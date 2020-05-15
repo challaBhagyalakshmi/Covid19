16e27bf88f0fb0adeeff09776029c92e3686492a
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:23:14 +0530

    export data from csv to countries table

--- src/data/processing/processing.js
+++ src/data/processing/processing.js
@@ -1,24 +1,27 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const countries = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/countries.js");
+const countries = require("../../db/Models/countries.js");
 const Country = countries.country;
-fs.createReadStream(
-  "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/countries.csv"
-)
-  .pipe(csv())
-  .on("data", row => {
-    const data = Object.values(row);
-    const dat = data[0];
-    Country.sync()
-      .then(function() {
-        return Country.create({
-          country_name: dat
+var count = 0;
+async function countries_data() {
+  fs.createReadStream("../csv_files/countries.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      const data = Object.values(row);
+      const dat = data[0];
+      Country.sync()
+        .then(function () {
+          return Country.create({
+            country_name: dat,
+          });
+        })
+        .then((data) => {
+          console.log(data.country_name);
         });
-      })
-      .then(data => {
-        console.log(data);
-      });
-  })
-  .on("end", () => {
-    console.log("csv file successfully processed");
-  });
+    })
+    .on("end", () => {
+      console.log("csv file successfully processed");
+    });
+}
+
+module.exports = { countries_data };
