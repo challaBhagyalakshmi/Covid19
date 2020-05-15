606cb7f441f489fa97cd02fd01764fb1e35484d3
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:21:17 +0530

    export data from csv to confirm_case table

--- src/data/processing/confirmed.js
+++ src/data/processing/confirmed.js
@@ -1,14 +1,11 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const foreign_key = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/Foreignkeys/confirm.js");
-const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/confirm.js");
+const confirm = require("../../db/Models/confirm.js");
 const Confirm = confirm.Confirm;
 
-Confirm.destroy({});
 async function confirm_data_import_db() {
-  fs.createReadStream(
-    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/confirmes.csv"
-  )
+  Confirm.destroy({ where: {}, truncate: true });
+  fs.createReadStream("../csv_files/confirmes.csv")
     .pipe(csv())
     .on("data", (row) => {
       const data = row["4/28/20"];
@@ -16,19 +13,20 @@ async function confirm_data_import_db() {
       Confirm.sync()
         .then(function () {
           return Confirm.create({
-            "4/28/20": data,
+            no_of_cases_till_yesterday: data,
             country_name: coun_name,
           });
         })
         .then((dat) => {
           console.log(JSON.stringify(dat));
         });
-      console.log(row);
-      foreign_key.foreign_confirm();
     })
     .on("end", () => {
       console.log("csv file successfully processed");
     });
 }
 
+confirm_data_import_db();
 module.exports = { confirm_data_import_db };
+
+ 
