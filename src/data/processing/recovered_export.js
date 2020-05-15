20e63461aae796186b1cb1c2fdcf19972925deed
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:24:09 +0530

    export data from csv to recovered_cases table

--- src/data/processing/recovered_export.js
+++ src/data/processing/recovered_export.js
@@ -1,13 +1,11 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const foreign_key = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/Foreignkeys/recover.js");
-const recovered = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/recovered.js");
+const recovered = require("../../db/Models/recovered.js");
 const Recover = recovered.Recover;
 
 async function recovered_data_import_db() {
-  fs.createReadStream(
-    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/recovered.csv"
-  )
+  Recover.destroy({ where: {}, truncate: true });
+  fs.createReadStream("../csv_files/recovered.csv")
     .pipe(csv())
     .on("data", (row) => {
       const data = row["4/28/20"];
@@ -15,16 +13,17 @@ async function recovered_data_import_db() {
       Recover.sync()
         .then(function () {
           return Recover.create({
-            "4/28/20": data,
+            no_of_cases_till_yesterday: data,
             country_name: coun_name,
           });
         })
         .then((dat) => {
           console.log(JSON.stringify(dat));
         });
-      foreign_key.foreign_key_recover();
     })
     .on("end", () => console.log("csv file successfully processed"));
 }
 
+recovered_data_import_db();
 module.exports = { recovered_data_import_db };
+
