db5fc0e464743b768058a6f9268e2aaf25cd11f0
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:45:00 +0530

    middlewares

--- src/api/Middlewares/app.js
+++ src/api/Middlewares/app.js
@@ -1,35 +1,43 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
-const confirm = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/confirm_router.js");
-const recover = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/recover_route.js");
-const deaths = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/deaths_route.js");
-const login = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/login.js");
-const signup = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/signup.js");
-const upload_data = require("/Users/bhagyalakshmi/Documents/COVID_19/src/api/routes/adminupload.js");
+const app = express();
+const confirm = require("../routes/confirm_router.js");
+const recover = require("../routes/recover_route.js");
+const deaths = require("../routes/deaths_route.js");
+const login = require("../routes/login.js");
+const signup = require("../routes/signup.js");
+const Admin = require("../routes/admin_upload");
+
+const Confirm = confirm.router;
+const Recover = recover.router;
+const Deaths = deaths.router;
+const Login = login.router;
+const Signup = signup.router;
+const admin = Admin.router;
+
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/confirmed", confirm);
-app.use("/recovered", recover);
-app.use("/deaths", deaths);
-app.use("/upload", upload_data);
-app.use("/users", signup);
-app.use("/users", login);
-app.use((req, res, next) => {
+app.use("/confirmed", Confirm);
+app.use("/recovered", Recover);
+app.use("/deaths", Deaths);
+app.use("/user", Signup);
+app.use("/users", Login);
+app.use("/admin", admin);
+app.use(async (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,DELETE");
   next();
 });
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   const error = new Error("Not Found");
   res.status = 404;
   next(error);
 });
-app.use((error, req, res, next) => {
+app.use(async (error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
     error: error.message,
