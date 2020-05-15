19eb860fa811327a9f7674cc1c52661b6e12e2ae
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-16 01:22:16 +0530

    export data from csv to deaths table

--- src/data/processing/deaths_export.js
+++ src/data/processing/deaths_export.js
@@ -1,14 +1,11 @@
 const csv = require("csv-parser");
 const fs = require("fs");
-const foreign_key = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/Foreignkeys/deaths.js");
-const death = require("/Users/bhagyalakshmi/Documents/COVID_19/src/db/Models/deaths.js");
+const death = require("../../db/Models/deaths.js");
 const deaths = death.Death;
 
-deaths.destroy({});
 async function deaths_data_import_db() {
-  fs.createReadStream(
-    "/Users/bhagyalakshmi/Documents/COVID_19/src/data/csv_files/deaths.csv"
-  )
+  deaths.destroy({ where: {}, truncate: true });
+  fs.createReadStream("../csv_files/deaths.csv")
     .pipe(csv())
     .on("data", (row) => {
       const data = row["4/28/20"];
@@ -17,18 +14,18 @@ async function deaths_data_import_db() {
         .sync()
         .then(function () {
           return deaths.create({
-            "4/28/20": data,
+            no_of_cases_till_yesterday: data,
             country_name: coun_name,
           });
         })
         .then((data) => {
           console.log(JSON.stringify(data));
         });
-      foreign_key.foreign_deaths();
     })
     .on("end", () => {
       console.log("csv file successfully processed");
     });
 }
 
+deaths_data_import_db();
 module.exports = { deaths_data_import_db };
