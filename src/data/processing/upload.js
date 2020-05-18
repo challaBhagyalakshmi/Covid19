b300ec04fd795b8ade5a9c178ff7f5a95a74ef34
Bhagyalakshmi <59639007+challaBhagyalakshmi@users.noreply.github.com>
2020-05-18 18:37:40 +0530

    upload data into db

--- src/data/processing/upload.js
+++ src/data/processing/upload.js
@@ -1,6 +1,7 @@
 const confirm = require("./confirmed");
 const recover = require("./recovered_export");
 const deaths = require("./deaths_export");
+const foreign = require("./Foreignkeys/foreign");
 
 async function main() {
   await confirm.confirm_data_import_db();
@@ -8,6 +9,10 @@ async function main() {
   await deaths.deaths_data_import_db();
 }
 
-main();
+async function upload() {
+  await main();
+  await foreign.main();
+}
 
+upload();
 module.exports = { main };
